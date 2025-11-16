1e990c18729bae699ed38b39956e33a471909577
test <1osama1adel1@gmail.com>
2025-11-16 20:14:37 +0300

    fix: move templates and add post_detail; setup media images

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add/", views.add_post, name="add_post"),
+    path("<int:post_id>/", views.post_detail, name="post_detail"),
+    path("<int:post_id>/edit/", views.edit_post, name="edit_post"),
+    path("<int:post_id>/delete/", views.delete_post, name="delete_post"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -6,12 +6,39 @@ def home(request):
     posts = Post.objects.filter(is_published=True).order_by("-published_at")
     return render(request, "posts/home.html", {"posts": posts})
 
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "posts/post_detail.html", {"post": post})
+
 def add_post(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("home")
     else:
         form = PostForm()
     return render(request, "posts/add_post.html", {"form": form})
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = PostForm(instance=post)
+    return render(request, "posts/edit_post.html", {"form": form, "post": post})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        post.delete()
+        return redirect("home")
+    return render(request, "posts/delete_post.html", {"post": post})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "posts/post_detail.html", {"post": post})
